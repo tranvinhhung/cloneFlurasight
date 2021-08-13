7d7b4f23eb4ac11832d49b50783174baa632a4b6
tranvinhhung <tranvinhung2k@gmail.com>
2021-08-13 14:10:54 +0700

    add animation and a litle reponsive

--- assets/Js/main.js
+++ assets/Js/main.js
@@ -1,10 +1,8 @@
 // let childUL = document.querySelector(".Home-1").children;
 let iClickTrans = document.querySelectorAll(".Home__category--item");
 let allIarrow = document.querySelectorAll(".Home i.fa-chevron-down");
-console.log(allIarrow);
-window.addEventListener("", function (e) {
-  console.log(e.clientX, e.clientY);
-});
+let mainE = document.getElementById("main");
+
 let index1; //index so sánh làm biếng để lưu cái index của cái home__Category--items mà mình click vô so sánh với cái click ở sau
 let transformIndex;
 let flatfon = document.querySelector(".platform");
@@ -68,6 +66,9 @@ iClickTrans.forEach((event, index) => {
       arr.map((e) => {
         if (e.parentElement.parentElement.classList.contains("hien")) {
           e.classList.add("transtrom180_acti");
+          mainE.classList.add("opacity__active");
+        } else {
+          mainE.classList.remove("opacity__active");
         }
       });
     });
@@ -122,9 +123,45 @@ setInterval(function () {
   transFormTextFunc();
 }, 3000);
 
+//scroll visibility element
+let getAllCoures = document.querySelectorAll(".animate");
+// console.log(getAllCoures);
+window.addEventListener("scroll", function () {
+  let getAllCoures = document.querySelectorAll(".animate");
+
+  let vh = window.innerHeight;
+
+  getAllCoures.forEach((e) => {
+    let topCoure = e.getBoundingClientRect().top;
+    let topPoint = (120 / 100) * vh;
+    if (topCoure < topPoint) {
+      console.log(topCoure, topPoint);
+      e.classList.add("active");
+    } else {
+      console.log(topCoure, topPoint);
+      e.classList.remove("active");
+    }
+  });
+});
 /*test setporerty click */
 
 ///cách get css property nè
 // console.log([flatfon]);
 const styleFlatform = getComputedStyle(flatfon);
 const displayFlatform = styleFlatform.display;
+
+//hieu ung doi anh slider dep
+let linkImage = [
+  "https://c.wallhere.com/photos/f7/82/futuristic_tech_geometry-1330193.jpg!d",
+  "https://images.unsplash.com/photo-1575761344173-aa186491c120?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
+  "https://images.unsplash.com/photo-1575761344173-aa186491c120?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
+  "https://images.unsplash.com/photo-1542640244-7e672d6cef4e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80",
+  "https://c.wallhere.com/photos/f7/82/futuristic_tech_geometry-1330193.jpg!d",
+];
+
+let imageInsert = document.querySelector(".slider__images");
+for (let i = 0; i < linkImage.length; i++) {
+  let imgSoure = `<img src=${linkImage[i]} alt="" />`;
+  console.log(imgSoure);
+  imageInsert.insertAdjacentHTML("afterbegin", imgSoure);
+}
